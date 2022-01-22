82b89acc9a6d41e66044af6781e20bd0fa215339
Bhavith-123 <97665580+Bhavith-123@users.noreply.github.com>
2022-01-22 14:32:30 +0530

    Update main.py

--- main.py
+++ main.py
@@ -6,27 +6,20 @@ app.secret_key = 'loginsessionwithb344’
 def index():
    if 'username' in session:
       username = session['username']
-         return 'Logged in as ' + username + '<br>' + "<b><a href = '/logout'>click here to log out</a></b>"
-   return "You are not logged in <br><a href = '/login'>" + "click here to log in</a>"
+         return render_template(url_for('index.html', username=username))
+   return render_template(url_for('login.html'))
 
 @app.route('/login', methods = ['GET', 'POST'])
 def login():
    if request.method == 'POST':
       session['username'] = request.form['username']
-      return redirect(url_for('index'))
-   return '''
-	
-   <form action = "" method = "post">
-      <p><input type = text name = username/></p>
-      <p<<input type = submit value = Login/></p>
-   </form>	
-'''
+      return redirect(url_for('index.html'))
+   return redirect(url_for('login.html'))
 
 @app.route('/logout')
 def logout():
-   # remove the username from the session if it is there
    session.pop('username', None)
-   return redirect(url_for('index'))
+   return redirect(url_for('index.html'))
 
 if __name__ == '__main__':
     app.run(debug=True)
